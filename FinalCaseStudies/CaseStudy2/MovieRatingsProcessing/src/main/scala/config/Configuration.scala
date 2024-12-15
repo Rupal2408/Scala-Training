65c6fb589009ade29b9c5161e538421228925a73
Rupal Gupta <rupal.gupta240897@gmail.com>
2024-12-15 15:23:19 +0530

    code cleanuo

--- FinalCaseStudies/CaseStudy2/MovieRatingsProcessing/src/main/scala/config/Configuration.scala
+++ FinalCaseStudies/CaseStudy2/MovieRatingsProcessing/src/main/scala/config/Configuration.scala
@@ -5,17 +5,17 @@ object Configuration {
   val kafkaBootstrapServers: String = sys.env.getOrElse("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
   val kafkaTopic: String = sys.env.getOrElse("KAFKA_TOPIC", "movie_ratings")
 
-  val gcsJsonKeyfile = "/Users/rupalgupta/gcp-final-key.json"
+  val gcsJsonKeyfile: String = sys.env.getOrElse("GCS_KEYFILE","/Users/rupalgupta/gcp-final-key.json")
 
   // Path to raw data in GCS
-  val moviesDataPath: String = sys.env.getOrElse("FEATURES_DATA_PATH", "gs://gcs_bucket_rupal/movies.csv")
-  val usersDataPath: String = sys.env.getOrElse("STORES_DATA_PATH", "gs://gcs_bucket_rupal/user_data.csv")
+  val moviesDataPath: String = sys.env.getOrElse("MOVIES_DATA_PATH", "gs://gcs_bucket_rupal/movies.csv")
+  val usersDataPath: String = sys.env.getOrElse("USERS_DATA_PATH", "gs://gcs_bucket_rupal/user_data.csv")
 
   // Path for storing aggregated metrics
-  val aggregatedMovieMetricsPath = "gs://gcs_bucket_rupal/case_study_2/aggregated_movie_metrics"
-  val aggregatedGenreMetricsPath = "gs://gcs_bucket_rupal/case_study_2/aggregated_genre_metrics"
-  val aggregatedUserDemographicsMetricsPath = "gs://gcs_bucket_rupal/case_study_2/aggregated_user_demographic_metrics"
-  val enrichedDataPath = "gs://gcs_bucket_rupal/case_study_2/enriched_ratings/"
+  val aggregatedMovieMetricsPath: String = sys.env.getOrElse("AGGREGATED_MOVIE_METRICS_PATH","gs://gcs_bucket_rupal/case_study_2/aggregated_movie_metrics")
+  val aggregatedGenreMetricsPath: String = sys.env.getOrElse("AGGREGATED_GENRE_METRICS_PATH","gs://gcs_bucket_rupal/case_study_2/aggregated_genre_metrics")
+  val aggregatedUserDemographicsMetricsPath: String = sys.env.getOrElse("AGGREGATED_USER_DEMO_PATH","gs://gcs_bucket_rupal/case_study_2/aggregated_user_demographic_metrics")
+  val enrichedDataPath: String = sys.env.getOrElse("ENRICHED_DATA_PATH","gs://gcs_bucket_rupal/case_study_2/enriched_ratings/")
 
 }
 
